c3beb1187f0f4da873fb54f58e14b73bb261d400
Sebastian Bento Inneo Veiga <sinneo@fi.uba.ar>
2022-02-27 16:37:41 -0300

    add part3a

--- part3/index.js
+++ part3/index.js
@@ -1,7 +1,17 @@
 const express = require('express')
 const app = express()
 
+const requestLogger = (request, response, next) => {
+	console.log('Method:', request.method)
+	console.log('Path:', request.path)
+	console.log('Body:', request.body)
+	console.log('---')
+	next()
+}
+
+
 app.use(express.json())
+app.use(requestLogger)
 
 let notes = [
 	{
@@ -75,6 +85,14 @@ app.post('/api/notes', (request, response) => {
 
 	response.json(note)
 })
+
+const unkownEndpoint = (request, response) => {
+	response.status(404).send({
+		error: "unkown endpoint"
+	})
+}
+
+app.use(unkownEndpoint)
 
 const PORT = 3001
 app.listen(PORT, () => {
