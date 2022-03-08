ebe39b1bddeeba99bd42285cd1472bb94153b76b
Sebastian Bento Inneo Veiga <sinneo@fi.uba.ar>
2022-03-08 20:07:28 -0300

    add part3c explanation

--- part3/index.js
+++ part3/index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const Note = require('./models/note')
 
 const requestLogger = (request, response, next) => {
 	console.log('Method:', request.method)
@@ -45,25 +47,29 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request, response) => {
-	response.json(notes)
+	Note.find({}).then(notes => {
+		response.json(notes)
+	})
 })
 
-app.get('/api/notes/:id', (request, response) => {
-	const id = Number(request.params.id)
-	const note = notes.find(note => note.id === id)
-	if (note) {
-		response.json(note)
-	} else {
-		response.status(404).end()
-	}
-	
+app.get('/api/notes/:id', (request, response, next) => {
+	Note.findById(request.params.id)
+	.then(note => {
+		if (note) {
+			response.json(note)
+		} else {
+			response.status(404).end()
+		}
+	})
+	.catch(error => next(error))
 })
 
 app.delete('api/notes/:id', (request, response) => {
-	const id = Number(request.params.id)
-	notes = notes.filter(note => note.id !== id)
-
-	response.status(204).end()
+	Note.findByIdAndRemove(request.params.id)
+		.then(result => {
+			response.status(204).end()
+		})
+		.catch(error => next(error))
 })
 
 app.post('/api/notes', (request, response) => {
@@ -75,16 +81,30 @@ app.post('/api/notes', (request, response) => {
 		})
 	}
 
-	const note = {
+	const note = new Note({
 		content: body.content,
 		important: body.important || false,
-		date: new Date(),
-		id: generateID()
+		date: new Date()
+	})
+
+	note.save().then(savedNote => {
+		response.json(savedNote)
+	})
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+	const body = request.body
+
+	const note = {
+		content: body.content,
+		important: body.important
 	}
 
-	notes = notes.concat(note)
-
-	response.json(note)
+	Note.findByIdAndUpdate(request.params.id, note, { new: true })
+		.then(updatedNote => {
+			response.json(updatedNote)
+		})
+		.catch(error => next(error))
 })
 
 const unkownEndpoint = (request, response) => {
@@ -95,7 +115,19 @@ const unkownEndpoint = (request, response) => {
 
 app.use(unkownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const errorHandler = (error, request, response, next) => {
+	console.error(error.message)
+
+	if (error.name === 'CastError') {
+		return response.status(400).send({ error: 'malformatting id' })
+	}
+
+	next(error)
+}
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`)
 })
